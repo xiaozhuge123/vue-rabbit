7b39774098b74d011c403bf9efcae156e348a2a8
xiaozhuge123 <xiaosasatuoge@163.com>
2025-09-02 04:06:11 +0800

    静态资源的创建

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+//引入初始化样式资源
+import '@/styles/common.scss'
+
 const app = createApp(App)
 
 //测试调用axios函数
