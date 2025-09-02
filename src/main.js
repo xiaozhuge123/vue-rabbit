874a72766b7f368acc5db09cef5d4dfa954938fa
xiaozhuge123 <xiaosasatuoge@163.com>
2025-09-03 04:39:23 +0800

    懒加载图片指令实现

--- src/main.js
+++ src/main.js
@@ -7,15 +7,12 @@ import router from './router'
 //引入初始化样式资源
 import '@/styles/common.scss'
 
-const app = createApp(App)
+// 全局指令注册
+import { lazyPlugin } from '@/directives'
 
-//测试调用axios函数
-// import {getCategary} from '@/apis/testApi.js'
-// getCategary().then(res=>{
-//   console.log(res)
-// })
+const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin)
 app.mount('#app')
