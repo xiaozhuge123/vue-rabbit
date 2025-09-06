777592dcf55f0290ee9d4ef9500d9902ddfa0187
xiaozhuge123 <xiaosasatuoge@163.com>
2025-09-06 18:37:34 +0800

    列表购物车单选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -37,12 +37,19 @@ export const useCartStore = defineStore('cart', () => {
   const allCount = computed(() => cartList.value.reduce((a,c) => a+c.count,0))
   const allPrice = computed(() => cartList.value.reduce((a,c) => a+c.count * c.price,0))
 
+  // 单选功能
+  const singleCheck = (skuId, selected) => {
+    // 通过skuId找到要修改的那一项 然后把它的selected修改为传过来的selected
+    const item = cartList.value.find((item) => item.skuId === skuId)
+    item.selected = selected
+  }
   return {
     cartList,
     addCart,
     delCart,
     allCount,
-    allPrice
+    allPrice,
+    singleCheck
   }
 }, {
   persist: true,
