e640cb76cded25e5cc064ac1aaa9c164694f32c7
xiaozhuge123 <xiaosasatuoge@163.com>
2025-09-06 10:58:51 +0800

    删除购物车和计算统计

---
+++ src/stores/cartStore.js
@@ -0,0 +1,49 @@
+// 封装购物车模块
+
+import { defineStore } from 'pinia'
+import { ref,computed } from 'vue'
+
+export const useCartStore = defineStore('cart', () => {
+  // 1. 定义state - cartList
+  const cartList = ref([])
+  // 2. 定义action - addCart
+  const addCart = (goods) => {
+    console.log('添加', goods)
+    // 添加购物车操作
+    // 已添加过 - count + 1
+    // 没有添加过 - 直接push
+    // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+    const item = cartList.value.find((item) => goods.skuId === item.skuId)
+    if (item) {
+      // 找到了
+      item.count++
+    } else {
+      // 没找到
+      cartList.value.push(goods)
+    }
+  }
+
+  // 删除购物车
+  const delCart = async (skuId) => {
+    // 思路：
+    // 1. 找到要删除项的下标值 - splice
+    // 2. 使用数组的过滤方法 - filter
+    const idx = cartList.value.findIndex((item) => skuId === item.skuId)
+    cartList.value.splice(idx, 1)
+  }
+
+  //计算属性
+  //计算商品总的数量
+  const allCount = computed(() => cartList.value.reduce((a,c) => a+c.count,0))
+  const allPrice = computed(() => cartList.value.reduce((a,c) => a+c.count * c.price,0))
+
+  return {
+    cartList,
+    addCart,
+    delCart,
+    allCount,
+    allPrice
+  }
+}, {
+  persist: true,
+})
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,10 @@ import DetailHot from "@/views/Detail/components/DetailHot.vue";
 import { getDetail } from '@/apis/detailApi.js'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import {ElMessage} from "element-plus";
+import {useCartStore} from '@/stores/cartStore.js'
+
+const cartStore = useCartStore()
 const goods = ref({})
 const route = useRoute()
 const getGoods = async () => {
@@ -11,8 +15,31 @@ const getGoods = async () => {
 }
 onMounted(() => getGoods())
 
+let skuObj = {}
 const skuChange = (sku) =>{
   console.log(sku)
+  skuObj = sku
+}
+const count = ref(1)
+const countChange = ()=>{
+  console.log(count.value)
+}
+//绑定购物车
+const addCart = ()=>{
+  if(skuObj.skuId){
+    cartStore.addCart({
+      id:goods.value.id,
+      name: goods.value.name,
+      picture: goods.value.mainPictures[0],
+      price: goods.value.price,
+      count: count.value,
+      skuId:skuObj.skuId,
+      attrsText: skuObj.specsText,
+      selected:true
+    })
+  }else {
+    ElMessage.warning("请选择规格")
+  }
 }
 </script>
 
@@ -93,10 +120,10 @@ const skuChange = (sku) =>{
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange"/>
               <!-- 数据组件 -->
-
+              <el-input-number v-model="count"  @change="countChange" />
               <!-- 按钮组件 -->
               <div>
-                <el-button size="large" class="btn">
+                <el-button size="large" class="btn" @click="addCart">
                   加入购物车
                 </el-button>
               </div>
