abcbda1edbf4bb2fcad819812973c606a1403616
xiaozhuge123 <xiaosasatuoge@163.com>
2025-09-07 16:03:15 +0800

    支付相关功能

--- src/views/pay/index.vue
+++ src/views/pay/index.vue
@@ -1,5 +1,27 @@
 <script setup>
-const payInfo = {}
+import { getOrderAPI } from '@/apis/payApi.js'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useCountDown } from '@/composables/useCountDown.js'
+
+const { start,formatTime } = useCountDown()
+// 获取订单数据
+const route = useRoute()
+const payInfo = ref({})
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value = res.result
+  start(res.result.countdown)
+}
+onMounted(() => getPayInfo())
+
+//跳转支付宝
+// 支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
+
 </script>
 
 
@@ -11,7 +33,7 @@ const payInfo = {}
         <span class="icon iconfont icon-queren2"></span>
         <div class="tip">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
         <div class="amount">
           <span>应付总额：</span>
